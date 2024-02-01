1d9b6fcd29eaa3a02f66f0f7fa187ad75ae4fcd9
Juanxtron <jpcanceladocaro@gmail.com>
2024-02-01 18:37:16 -0500

    Funcion dist normal

--- codigo.py
+++ codigo.py
@@ -1,7 +1,4 @@
 import numpy as np
-mu = 3
-sigma = 0.5 
-n=1000
 numeros = np.random.chisquare(df=3,size=1000)
 import matplotlib.pyplot as plt
 count, bins, ignored = plt.hist(x=numeros, bins=30)
@@ -10,3 +7,20 @@ plt.xlabel('Tiempos de servicio')
 plt.ylabel('Frecuencia')
 plt.show()
 
+#Se crea una grafica con una dstribución normal y una linea de densidad.
+mu = 3
+sigma = 0.5 
+n = 1000
+
+numeros = np.random.normal(mu, sigma, n)
+
+# Graficar histograma
+count, bins, ignored = plt.hist(x=numeros, bins=30, density=True, color='blue', alpha=0.7)
+
+# Agregar línea de densidad de probabilidad
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-(bins - mu)**2 / (2 * sigma**2)), linewidth=2, color='red')
+
+plt.title('Histograma de tiempos de servicio (Distribución Normal)')
+plt.xlabel('Tiempos de servicio')
+plt.ylabel('Frecuencia')
+plt.show()
